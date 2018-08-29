77690db999425c2f739938281a54af34ddc5726e
Alpha Diallo <alphadiallo009@gmail.com>
2018-08-29 20:40:47 +0200

    Redux 1
    Redux 1

--- Components/API/TMDBApi.js
+++ Components/API/TMDBApi.js
@@ -1,6 +1,7 @@
 const API_TOKEN = "d28e0af1fb360e1558da665e441dec52"
 
 
+//Fetch from URL
 export function getFilmsFromApiWithSearchedText (text, page) {
   const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + '&page='+page
 
